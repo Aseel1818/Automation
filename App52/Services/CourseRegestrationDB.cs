53cbdd957806f216d60629d36bef099cc057fb6b
Aseel1818 <63041611+Aseel1818@users.noreply.github.com>
2022-05-10 13:19:36 +0300

    Add files via upload

---
+++ App52/Services/CourseRegestrationDB.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+using System.Threading.Tasks;
+using App52.Model;
+using Firebase.Database;
+using Firebase.Database.Query;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Linq;
+using System.Windows;
+namespace App52.Services
+{
+    public class CourseRegestrationDB
+    {
+        FirebaseClient client;
+
+        public CourseRegestrationDB()
+        {
+            client = new FirebaseClient("https://capregestration-default-rtdb.firebaseio.com/");
+
+        }
+        public ObservableCollection<CourseRegisterModel> getStudent()
+        {
+            var StuedntData = client.Child("CourseRegestration").AsObservable<CourseRegisterModel>().AsObservableCollection();
+            
+
+
+            return StuedntData;
+
+
+
+        }
+
+       
+
+        public async Task AddStudent(string sName, string Snum, string Cnum, string Divisionnumber, string Description)
+        {
+            CourseRegisterModel c =new CourseRegisterModel() { SName= sName, snum=Snum,cnum=Cnum,divisionnumber=Divisionnumber,description=Description };
+            await client.Child("CourseRegestration").PostAsync(c);
+        }
+
+    }
+}
---
+++ App52/Views/CourseRegister.xaml.cs
@@ -0,0 +1,79 @@
+﻿using App52.ModelView;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App52.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class CourseRegister : ContentPage
+    {
+        public CourseRegister()
+        {
+            InitializeComponent();
+            BindingContext = new CourseRegisterModelView();
+        }
+
+
+        private async void Submit_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                string name = StudentName.Text;
+                string num = StudentNumber.Text;
+                string coursNumber = CourseNumber.Text;
+                string DivNumber = divisionNumber.Text;
+                string Description = description.Text;
+
+
+
+
+                if (string.IsNullOrEmpty(name))
+                {
+                    await DisplayAlert("Warning", "Please enter Your Name  ", "Cancel");
+                    return;
+                }
+
+                if (string.IsNullOrEmpty(num))
+                {
+                    await DisplayAlert("Warning", "Please enter Your Number  ", "Cancel");
+                    return;
+                }
+
+
+                if (string.IsNullOrEmpty(coursNumber))
+                {
+                    await DisplayAlert("Warning", "Please enter your course Number ", "Cancel");
+                    return;
+                }
+                if (string.IsNullOrEmpty(DivNumber))
+                {
+                    await DisplayAlert("Warning", "Please enter your  Division Number ", "Cancel");
+                    return;
+                }
+
+                if (string.IsNullOrEmpty(Description))
+                {
+                    await DisplayAlert("Warning", "Please enter Description ", "Cancel");
+                    return;
+                }
+
+
+
+            }
+            catch (Exception exception)
+            {
+                if (exception.Message.Contains("INVALID_EMAIL"))
+                {
+                    await DisplayAlert("Warning", "EMAIL_EXISTS", "ok");
+                }
+
+            }
+        }
+    }
+}
